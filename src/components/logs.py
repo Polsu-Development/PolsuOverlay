f6e5d3fa61a8feb5711144e6016c19d8490d5ad7
zickles <zicklesistaken@gmail.com>
2023-12-27 13:44:40 -0500

    readd win to notif

--- src/components/logs.py
+++ src/components/logs.py
@@ -155,7 +155,7 @@ class Logs:
             return new
         except FileNotFoundError:
             if not self.error_sent:
-                self.notif.send(
+                self.win.notif.send(
                     title="Warning!",
                     message="The log file you are currently using isn't valid.\nGo to: Settings -> Client, and choose a valid client.",
                     block=True
